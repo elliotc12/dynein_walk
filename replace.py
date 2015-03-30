6982290b6614f1af5c065cca49b4341f4a6643c2
Elliott Capek <elliotc12@wentworth.physics.oregonstate.edu>
2015-03-30 12:48:45 -0700

    Added progress print statements to replace.py

--- replace.py
+++ replace.py
@@ -30,7 +30,7 @@ text = re.sub(r'Derivative\[2\]\[mla\]\[t\] -\>', 'MLA_DERIVATIVE_2: ', text)
 text = re.sub(r'Derivative\[2\]\[mra\]\[t\] -\>', 'MRA_DERIVATIVE_2: ', text)
 text = re.sub(r'Derivative\[2\]\[bra\]\[t\] -\>', 'BRA_DERIVATIVE_2: ', text)
 
-
+print "replace.py: Changing Mathematica symbols to C symbols"
 text = re.sub('\n', '', text)															# Remove all line breaks
 text = re.sub(r'[ ]{2,}', '', text)			 											# Delete all large spaces
 text = re.sub(r'(\([a-z]*)\^\\\[Prime\]\\\[Prime\]\)\[t\] \-\>', r'\n\1":\n', text)		# Change 'Derivative[1]...' to 'get_d_...'
@@ -46,6 +46,7 @@ text = re.sub(r'([a-zA-Z]+)\^2', r'square(\1)', text)									# Change ^2/^3 to 
 text = re.sub(r'([a-zA-Z]+)\^3', r'cube(\1)', text)
 text = re.sub(r'([a-zA-Z]+)\^5', r'fifth(\1)', text)
 
+print "replace.py: Changing Mathematica variable names to C variable names"
 text = re.sub('Lt', 'lt', text)			# Convert from Mathematica variable naming scheme to C variable naming scheme
 text = re.sub('Ls', 'ls', text)
 text = re.sub('mA', 'ma', text)
@@ -56,6 +57,7 @@ text = re.sub(r'F([mbrl]{2})y', r'f\1y', text)
 text = re.sub('Ftx', 'ftx', text)
 text = re.sub('Fty', 'fty', text)
 
+print "replace.py: Changing Mathematica squares / cubes to C squares / cubes"
 idx = 0
 while string.find(text, ")^2") != -1: # Change ^2/^3 to local square/cube functions for multi-variable expressions	
 	idx = string.find(text, ")^2")
@@ -108,11 +110,13 @@ while string.find(text, ")^3") != -1:
 	else:
 		text = text[:i] + "cube" + text[i:idx+1] + text[idx+3:]
 
+print "replace.py: Adding multiplication symbols"
 text = re.sub(r'([^ \t\r\f\v\-\+\\\*\-\>]+)[ \t\r\f\v]+([^ \t\r\f\v\-\+\\\*\-\>]+)', r'\1 * \2', text) # a b c d -> a * b c * d
 text = re.sub(r'([^ \t\r\f\v\-\+\\\*\-\>]+)[ \t\r\f\v]+([^ \t\r\f\v\-\+\\\*\-\>]+)', r'\1 * \2', text) # a * b c * d -> a * b * c * d
 text = re.sub(r'([0-9]+)', r'\1.0', text)
 text = re.sub(r'([\+-]{1})', r'\1 ', text)
 
+print "replace.py: Creating CPP file"
 text = re.sub(r'BLA_DERIVATIVE_2\.0:([^,\n]*),', r'bla: \1\n', text)		# Build temp structure to hold different expressions
 text = re.sub(r'MLA_DERIVATIVE_2\.0:([^,\n]*),', r'mla: \1\n', text)
 text = re.sub(r'MRA_DERIVATIVE_2\.0:([^,\n]*),', r'mra: \1\n', text)
