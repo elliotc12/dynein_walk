a670f126fc3615ecb0c98dfff92c9844e715c252
elliotc12 <elliotc12@gmail.com>
2015-08-12 11:27:15 -0700

    Add rudimentary tests and notes for more tests to write.

--- dynein_test.cpp
+++ dynein_test.cpp
@@ -11,6 +11,20 @@ int equal(float v1, float v2) {
 	else return fabs(v1 - v2)/fabs(v1) <= EPSILON;
 }
 
+int test(float one, float two) {
+
+  if (equal(one, two))
+  {
+    printf("yes, struct says %f and calculation says %f.\n", one, two);
+    return 0;
+  }
+  else 
+  {
+    printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
+    exit(EXIT_FAILURE);
+  }
+}
+
 int main() {
 	
 	float one, two;
@@ -28,131 +42,66 @@ int main() {
 	f.f_mrx = 0;     f.f_mry = 0;
 	f.f_brx = 0;     f.f_bry = 0;
 	
-	printf("\n\nRunning dynein_struct member value check...\n\n");
+	printf("\n\nRunning dynein_struct member value check...\n");
 	
-	//Dynein* dyn = new Dynein(( 90 / 180)*M_PI, ( 90 / 180)*M_PI, ( -90 / 180)*M_PI, ( -90 / 180)*M_PI, f);
+	// Dynein in normal pentagon conformation, do the velocities agree with the velocity definitions?
 	Dynein* dyn = new Dynein((108.0 / 180) * M_PI, (36.0 / 180) * M_PI, -(144.0 / 180) * M_PI, -(72.0 / 180) * M_PI, f);
 	
 	printf("\tIs d_blx defined properly..........");
 	one = dyn->get_d_blx();
 	two = 0;
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
+	test(one, two);
 	
 	printf("\tIs d_bly defined properly..........");
 	one = dyn->get_d_bly();
 	two = 0;
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
+        test(one, two);
 	
 	printf("\tIs d_mlx defined properly..........");
 	one = dyn->get_d_mlx();
 	two = -ls*sin(dyn->get_bla())*dyn->get_d_bla();
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
+	test(one, two);
 	
 	printf("\tIs d_mly defined properly..........");
 	one = dyn->get_d_mly();
 	two = ls*cos(dyn->get_bla())*dyn->get_d_bla();
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
+	test(one, two);
 	
 	printf("\tIs  d_tx defined properly..........");
 	one = dyn->get_d_tx();
 	two = -ls*sin(dyn->get_bla())*dyn->get_d_bla() + -lt*sin(dyn->get_mla())*dyn->get_d_mla();
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
+	test(one, two);
 	
 	printf("\tIs  d_ty defined properly..........");
 	one = dyn->get_d_ty();
 	two = ls*cos(dyn->get_bla())*dyn->get_d_bla() + lt*cos(dyn->get_mla())*dyn->get_d_mla();
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
+	test(one, two);
 	
 	printf("\tIs d_mrx defined properly..........");
 	one = dyn->get_d_mrx();
 	two = -ls*sin(dyn->get_bla())*dyn->get_d_bla() + -lt*sin(dyn->get_mla())*dyn->get_d_mla() + lt*sin(dyn->get_mra())*dyn->get_d_mra();
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
+	test(one, two);
 	
 	printf("\tIs d_mry defined properly..........");
 	one = dyn->get_d_mry();
 	two = ls*cos(dyn->get_bla())*dyn->get_d_bla() + lt*cos(dyn->get_mla())*dyn->get_d_mla() + -lt*cos(dyn->get_mra())*dyn->get_d_mra();
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
+	test(one, two);
 	
 	printf("\tIs d_brx defined properly..........");
 	one = dyn->get_d_brx();
 	two = -ls*sin(dyn->get_bla())*dyn->get_d_bla() + -lt*sin(dyn->get_mla())*dyn->get_d_mla() + lt*sin(dyn->get_mra())*dyn->get_d_mra() + ls*sin(dyn->get_bra())*dyn->get_d_bra();
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
+	test(one, two);
 	
 	printf("\tIs d_bry defined properly..........");
 	one = dyn->get_d_bry();
 	two = ls*cos(dyn->get_bla())*dyn->get_d_bla() + lt*cos(dyn->get_mla())*dyn->get_d_mla() + -lt*cos(dyn->get_mra())*dyn->get_d_mra() + -ls*cos(dyn->get_bra())*dyn->get_d_bra();
-	if (equal(one, two))
-	{
-		printf("yes, struct says %f and calculation says %f.\n", one, two);
-	} else 
-	{
-		printf("no! struct says %f and calculation says %f. Exiting.\n\n", one, two);
-		exit(EXIT_FAILURE);
-	}
-	
+	test(one, two);
+
+	// TODO: test other weird states
+	// Vertical dynein with only f_brx nonzero: all velocities zero except bra
+	// Horizontal dynein with only f_bry nonzero: all velocities zero except bra
+
+	printf("All tests pass!\n\n");
 	exit(EXIT_SUCCESS);
 }
 
