55ad46a8489696cd8c07e562f5a93082737422ce
David Roundy <roundyd@physics.oregonstate.edu>
2016-01-22 14:06:16 -0800

    cleanups around avoiding void* and typecasts in walk

--- dynein_walk.cpp
+++ dynein_walk.cpp
@@ -13,8 +13,9 @@
 extern const double dt;
 double runtime;
 
-void simulateProtein(void* dyn, double tf) {
+void simulateProtein(Dynein_onebound* dyn1, double tf) {
   double t = 0;
+  Dynein_bothbound *dynB = 0;
 
   FILE* data_file = fopen("data.txt", "a+");
   FILE* run_file = fopen("run.txt", "a+");
@@ -22,71 +23,66 @@ void simulateProtein(void* dyn, double tf) {
   MTRand* rand = new MTRand(RAND_INIT_SEED);
 
   while( t < tf ) {
-             // onebound case
-    if (((Dynein_onebound*) dyn)->get_state() != BOTHBOUND) { // fix type
-             // unbind
-      if (rand->rand() < ((Dynein_onebound*) dyn)->get_unbinding_rate()*dt) {
-	printf("unbinding.");
-	((Dynein_onebound*) dyn)->log_run(t, run_file);
-	((Dynein_onebound*) dyn)->log(t, data_file);
-	exit(EXIT_SUCCESS);
-      }
-      else if (rand->rand() < ((Dynein_onebound*) dyn)->get_binding_rate()*dt) {
-	     // switch to bothbound
-	Dynein_bothbound* new_dynein
-	  = new Dynein_bothbound(((Dynein_onebound*) dyn), rand);
-	free(((Dynein_onebound*) dyn));
-	dyn = new_dynein;
-      }
-      else { // move like normal
-	double temp_bba,temp_bma, temp_uma, temp_uba;
-	temp_bba = ((Dynein_onebound*) dyn)->get_bba() + ((Dynein_onebound*) dyn)->get_d_bba() * dt;
-	temp_bma = ((Dynein_onebound*) dyn)->get_bma() + ((Dynein_onebound*) dyn)->get_d_bma() * dt;
-	temp_uma = ((Dynein_onebound*) dyn)->get_uma() + ((Dynein_onebound*) dyn)->get_d_uma() * dt;
-	temp_uba = ((Dynein_onebound*) dyn)->get_uba() + ((Dynein_onebound*) dyn)->get_d_uba() * dt;
+    while (t < tf) { // loop as long as it is onebound
+      if (rand->rand() < dyn1->get_unbinding_rate()*dt) {
+        // this is the case where we fall off and become zerobound!
+        printf("unbinding.");
+        dyn1->log_run(t, run_file);
+        dyn1->log(t, data_file);
+        return;
+      } else if (rand->rand() < dyn1->get_binding_rate()*dt) {
+        // switch to bothbound
+        dynB = new Dynein_bothbound(dyn1, rand);
+        delete dyn1;
+        break;
+      } else { // move like normal
+        double temp_bba = dyn1->get_bba() + dyn1->get_d_bba() * dt;
+        double temp_bma = dyn1->get_bma() + dyn1->get_d_bma() * dt;
+        double temp_uma = dyn1->get_uma() + dyn1->get_d_uma() * dt;
+        double temp_uba = dyn1->get_uba() + dyn1->get_d_uba() * dt;
 
-	((Dynein_onebound*) dyn)->set_bba(temp_bba);
-	((Dynein_onebound*) dyn)->set_bma(temp_bma);
-	((Dynein_onebound*) dyn)->set_uma(temp_uma);
-	((Dynein_onebound*) dyn)->set_uba(temp_uba);
+        dyn1->set_bba(temp_bba);
+        dyn1->set_bma(temp_bma);
+        dyn1->set_uma(temp_uma);
+        dyn1->set_uba(temp_uba);
       }
-
-      ((Dynein_onebound*) dyn)->update_velocities();
-      ((Dynein_onebound*) dyn)->log(t, data_file);
+      dyn1->update_velocities();
+      dyn1->log(t, data_file);
+      t += dt;
     }
-    else {
-             // bothbound case
-      if (rand->rand() < ((Dynein_bothbound*) dyn)->get_near_unbinding_rate()*dt) {
-	     // switch to farbound
-	Dynein_onebound* new_dynein
-	  = new Dynein_onebound(((Dynein_bothbound*) dyn), rand, FARBOUND);
-	free(((Dynein_bothbound*) dyn));
-	dyn = new_dynein;
+    while (t < tf) { // loop as long as it is bothbound
+      bool unbind_near = rand->rand() < dynB->get_near_unbinding_rate()*dt;
+      bool unbind_far = rand->rand() < dynB->get_far_unbinding_rate()*dt;
+      if (unbind_near && unbind_far) {
+        printf("THEY BOTH WANT TO FALL OFF TOGETHER!!!\n");
+        printf("WHAT SHOULD THEY DO????\n");
       }
-      else if (rand->rand() < ((Dynein_bothbound*) dyn)->get_far_unbinding_rate()*dt) {
-	     // switch to nearbound
-	Dynein_onebound* new_dynein
-	  = new Dynein_onebound(((Dynein_bothbound*) dyn), rand, NEARBOUND);
-	free(((Dynein_bothbound*) dyn));
-	dyn = new_dynein;
-      }
-      else {
-	double temp_nma, temp_fma;
-	temp_nma = ((Dynein_bothbound*) dyn)->get_nma() + ((Dynein_bothbound*) dyn)->get_d_nma()*dt;
-	temp_fma = ((Dynein_bothbound*) dyn)->get_fma() + ((Dynein_bothbound*) dyn)->get_d_fma()*dt;
+      if (unbind_near) {
+        // switch to farbound
+	dyn1 = new Dynein_onebound(dynB, rand, FARBOUND);
+	delete dynB;
+        break;
+      } else if (unbind_far) {
+        // switch to nearbound
+	dyn1 = new Dynein_onebound(dynB, rand, NEARBOUND);
+	delete dynB;
+        break;
+      } else {
+	double temp_nma = dynB->get_nma() + dynB->get_d_nma()*dt;
+	double temp_fma = dynB->get_fma() + dynB->get_d_fma()*dt;
 
-	((Dynein_bothbound*) dyn)->set_nma(temp_nma);
-	((Dynein_bothbound*) dyn)->set_fma(temp_fma);
+	dynB->set_nma(temp_nma);
+	dynB->set_fma(temp_fma);
       }
 
-      ((Dynein_bothbound*) dyn)->update_velocities();
-      ((Dynein_bothbound*) dyn)->log(t, data_file);
+      dynB->update_velocities();
+      dynB->log(t, data_file);
+      t += dt;
     }
 
-    t += dt;
   }
 
-  ((Dynein_onebound*) dyn)->log_run(tf, run_file); // fix type
+  // dyn1->log_run(tf, run_file); // fix type
 
   fclose(data_file);
 }
@@ -120,6 +116,5 @@ int main(int argvc, char **argv) {
   resetLog();
   simulateProtein(dyn, runtime);
   delete dyn;
-  dyn = NULL;
   return 0;
 }
