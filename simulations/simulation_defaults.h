5ef8ec1db231ede3c5add9a5deab12485213c4e4
elliotc12 <elliotc12@gmail.com>
2016-07-30 04:07:41 -0700

    not a lot!

--- simulations/simulation_defaults.h
+++ simulations/simulation_defaults.h
@@ -1,8 +1,10 @@
+const long long iterations = 500;
+const int data_generation_skip_iterations = 1;
+
 const int num_corr_datapoints = 1000;
-const int data_generation_skip_iterations = 1e5;
 const double tau_runtime_fraction = 1e-5;
 const int movie_num_frames = 1e2;
 
-const int num_generate_pe_datapoints = 1e3;
-const int num_generate_angle_datapoints = 1e3;
-const int generate_averaging_width = 5e4;
+const int num_generate_pe_datapoints = 100;
+const int num_generate_angle_datapoints = 100;
+const int generate_averaging_width = 1;
