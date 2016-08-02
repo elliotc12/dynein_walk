cf783ec1536a2edaed61cd13e841fd0777e6fb26
David Roundy <roundyd@physics.oregonstate.edu>
2016-08-01 17:54:55 -0700

    set number of angle and PE datapoints always equal

--- simulations/simulation_defaults.h
+++ simulations/simulation_defaults.h
@@ -8,5 +8,5 @@ const double tau_runtime_fraction = 1e-5;
 const int movie_num_frames = 1e2;
 
 const int num_generate_pe_datapoints = 50;
-const int num_generate_angle_datapoints = 50;
+const int num_generate_angle_datapoints = num_generate_pe_datapoints;
 const int generate_averaging_width = 1;
