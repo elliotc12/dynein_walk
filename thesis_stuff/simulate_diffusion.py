ccde15f275737a4771e68facd7680650088c7138
elliotc12 <elliotc12@gmail.com>
2016-08-22 17:58:09 -0700

    Fix up simulate_diffusion.py for generating diffusion figure.

--- thesis_stuff/simulate_diffusion.py
+++ thesis_stuff/simulate_diffusion.py
@@ -3,8 +3,17 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
+runtime = 1
+dt = 1e-3
 
+radius = 1e-8
+cell_viscosity = 7e-4
+gamma = radius * 6.0 * np.pi * cell_viscosity
+
+kb = 1.38e-23
+T = 310.15
 
 def get_cell_potential_gradient(pos):
     return np.array([0, 0, 0])
@@ -18,34 +27,36 @@ def simulate_particle(init_position):
         R = np.random.normal(0, 2*kb*T*gamma/dt, 3)
         v = F/gamma + R
         pos += v*dt
-        if int(t/dt) % 100 == 0:
+        if int(t/dt) % 10000 == 0:
             print "Position: " + str(pos) +  "completion: " + str(t / runtime)
         trajectory.append(pos.copy())
         t += dt
     return trajectory
 
-def make_gif(trajectories):
+def make_gif(trajectory):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    for trajectory in trajectories:
-        xs = [point[0] for point in trajectory]
-        ys = [point[1] for point in trajectory]
-        zs = [point[2] for point in trajectory]
+    for t in range(len(trajectory)):
+        xs = [point[0] for (n, point) in enumerate(trajectory) if n < t] # = (trajectory[0:t])[0]
+        ys = [point[1] for (n, point) in enumerate(trajectory) if n < t] # = (trajectory[0:t])[1]
+        zs = [point[2] for (n, point) in enumerate(trajectory) if n < t] # = (trajectory[0:t])[2]
         ax.plot(xs, ys, zs)
 
-    # ax.set_xlim([-radius, radius])
-    # ax.set_ylim([-radius, radius])
-    # ax.set_zlim([-radius, radius])
-    plt.title("Diffusion trajectories")
-    plt.show()
+        ax.set_xlim([-1e-30, 1e-30])
+        ax.set_ylim([-1e-30, 1e-30])
+        ax.set_zlim([-1e-30, 1e-30])
+
+        plt.title("Diffusion trajectory")
+        plt.savefig('PNGs/diffusion-%03d.png' % t)
+        plt.cla()
+        print "Making fig: %d of %d" % (t, len(trajectory))
+    os.system("convert -delay 10 PNGs/diffusion-*.png diffusion.gif")
+    
 
 def main():
-    trajectories = []
-    for n in range(N):
-        init_position = np.array([0.0, 0.0, 0.0])
-        trajectory = simulate_particle(init_position)
-        trajectories.append(trajectory)
-    make_gif(trajectories)
+    init_position = np.array([0.0, 0.0, 0.0])
+    trajectory = simulate_particle(init_position)
+    make_gif(trajectory)
 
 if __name__ == "__main__":
     main()
