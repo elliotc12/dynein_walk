1a9de01d5b2cd6e25aade5a84a47581c7272bfe4
elliotc12 <elliotc12@gmail.com>
2016-09-02 12:27:18 -0700

    Fix makefile so data files only get made once.

--- simulations/simulation_defaults.h
+++ simulations/simulation_defaults.h
@@ -1,7 +1,7 @@
 #ifndef SIMULATION_DEFAULTS_H
 #define SIMULATION_DEFAULTS_H
 
-const long long iterations = 1e7;
+const long long iterations = 1e8;
 const long long data_generation_skip_iterations = 1e5;
 
 #endif
