370e5dc85705eadb299ec9f449098b43b1f18b09
elliotc12 <elliotc12@gmail.com>
2017-04-25 16:03:03 -0700

    Fix up trajectory plot.

--- trajectory-plt.py
+++ trajectory-plt.py
@@ -15,6 +15,8 @@ Usage: python2 TITLE %s [show] [tail]"
 	     omitting show makes %s faster but less exciting to watch
 ''' % (sys.argv[0], sys.argv[0])
 
+print("argv: ", sys.argv)
+
 if len(sys.argv) < 2:
   print usage
   sys.exit(1)
@@ -22,21 +24,19 @@ if len(sys.argv) < 2:
 data_filename = sys.argv[1]
 title = data_filename[data_filename.index("data/")+5:-4]
 
-if sys.stdin.isatty():
-    if tail:
-	data = np.genfromtxt(data_filename, delimiter="\t", invalid_raise=False, skip_header=skiplen)
-    else:
-	data = np.genfromtxt(data_filename, delimiter="\t", invalid_raise=False)
+if tail:
+    data = np.genfromtxt(data_filename, delimiter="\t", invalid_raise=False, skip_header=skiplen)
 else:
-  data = np.genfromtxt(sys.stdin, delimiter="\t", invalid_raise=False)
+    data = np.genfromtxt(data_filename, delimiter="\t", invalid_raise=False)
 
 if tail and sys.stdin.isatty():
     skiplen = sum(1 for line in open(data_filename)) - 100
     if skiplen < 0:
 	skiplen = 1
 
-if len(data) == 0 or str(type(data[0])) == "<type 'np.float64'>":
+if len(data) == 0:
        print "Very short run!"
+       exit(1)
 
 nbxs = data[:,7]
 fbxs = data[:,15]
@@ -46,9 +46,9 @@ x_max = np.max([np.max(nbxs), np.max(fbxs)])
 
 plt.title("Trajectories of two feet")
 plt.xlabel("time (s)")
-plt.ylabel("x position")
+plt.ylabel("foot position")
 
-plt.gca().set_xlim(x_min-1,x_max+1)
+plt.gca().set_ylim(x_min-1,x_max+1)
 
 plt.plot(nbxs, label="nbx")
 plt.plot(fbxs, label="fbx")
