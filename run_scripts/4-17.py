b1ef64344919eb7ccdac859bc903b813d4c0bef6
elliotc12 <elliotc12@gmail.com>
2017-04-24 20:27:02 -0700

    Minor change to sim scripts.

--- run_scripts/4-17.py
+++ run_scripts/4-17.py
@@ -2,7 +2,7 @@
 import simrunner
 import numpy as np
 
-l = "4-21"
+l = "4-23"
 
 simrunner.run_sim(**{"k_b": 1e-6, "k_ub":  1e5, "cb": 2.4, "cm": 2.4, "ct": 2.4, "dt": 1e-10, "label": l})
 simrunner.run_sim(**{"k_b": 1e-6, "k_ub":  1e6, "cb": 2.4, "cm": 2.4, "ct": 2.4, "dt": 1e-10, "label": l})
--- run_scripts/4-22.py
+++ run_scripts/4-22.py
@@ -2,7 +2,7 @@
 import simrunner
 import numpy as np
 
-l = "4-22"
+l = "4-23"
 
 simrunner.run_sim(**{"k_b": 1e-5, "k_ub":  1e2, "cb": 2.4, "cm": 2.4, "ct": 2.4, "dt": 1e-10, "label": l})
 simrunner.run_sim(**{"k_b": 1e-5, "k_ub":  1e3, "cb": 2.4, "cm": 2.4, "ct": 2.4, "dt": 1e-10, "label": l})
