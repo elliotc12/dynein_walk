326b15c2dfdbac4f39c05d3fb21c7d0b305596ad
elliotc12 <elliotc12@gmail.com>
2017-05-16 13:03:17 -0700

    Another minor fix

--- run_scripts/generate-thesis-data.py
+++ run_scripts/generate-thesis-data.py
@@ -6,7 +6,7 @@ l = "thesis"
 
 simrunner.avoid_slurm = True
 
-basename = simrunner.run_sim(**{"k_b": 1e4, "k_ub": 1e8, "cb": 2, "cm": 2, "ct": 1, "dt": 1e-10, "label": l,
+basename = simrunner.run_sim(**{"k_b": 1e16, "k_ub": 1e20, "cb": 2, "cm": 2, "ct": 1, "dt": 1e-10, "label": l,
                                 "seed": 8,
                                 "runtime": 1e-3,
                                 "constant-write": True,
