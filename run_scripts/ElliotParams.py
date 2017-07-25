876bde37db73f97c73e32abafb309c68ca4b5671
John Waczak <waczakj@oregonstate.edu>
2017-07-25 07:51:51 -0700

    simulation for Elliott's thesis parameters

---
+++ run_scripts/ElliotParams.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import simrunner
+import numpy as np
+
+l = "Elliott-Parameters"
+
+simrunner.run_sim(**{"k_b": 10e18, "k_ub": 10e20, "cb": 2.0, "cm": 2.0, "ct": 1.0, "dt": 1e-10, "label": l})
