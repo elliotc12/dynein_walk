580560f958bf23793a18b19b51390a10052a3a90
John Waczak <waczakj@oregonstate.edu>
2017-08-02 09:31:59 -0700

    runscript for dt = 1e-11 autocorr

---
+++ run_scripts/autocorr_dt1e-11.py
@@ -0,0 +1,13 @@
+#!/user/bin/python
+
+import simrunner
+import numpy as np
+
+l = "autocorr_dt_1e-10"
+
+simrunner.run_sim(**{"k_b": 10e20, "k_ub": 10e-10, "cb": 2.0, "cm": 2.0, "ct": 1.0, "dt": 1e-11, "label": l,
+                     "constant-write": True, "runtime": 1e-4})
+
+simrunner.run_sim(**{"k_b": 10e-10, "k_ub": 10e20, "cb": 2.0, "cm": 2.0, "ct": 1.0, "dt": 1e-11, "label": l,
+                     "constant-write": True, "runtime": 1e-4})
+
