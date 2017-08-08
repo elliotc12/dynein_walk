02ec05ddf12c07287aa98dba630254abe5c57c36
David Roundy <roundyd@physics.oregonstate.edu>
2017-08-08 13:09:31 -0700

    add faster autocorr

--- timeCorrelationTests/autocorrelation.py
+++ timeCorrelationTests/autocorrelation.py
@@ -3,25 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys 
 
-## autocorrelation fucntion definitions ##
-def autoCorrelate1(data):
-    rho = np.zeros(len(data)) # autocorrelation function to be returned
-    n = len(data)
-    mu = np.mean(data) #mean 
-    sig2 = np.var(data) #variance
-
-    #note: need to check range function for final index
-    for k in range(0, n): #index the k's up to n-1
-        R = 0
-        for t in range(0, n-k): #index t's up to n-k 
-            R = R + (data[t]-mu)*(data[t+k]-mu)
-
-        rho[k] = (1/((n-k)*sig2))*R
-    return rho 
-
-
 # use this method -- autoCorrelate1 was used to verify the math was correct
-def autoCorrelate2(data, Nmax = None, skipIndex = 0, verbose = False):
+def autoCorrelate1(data, Nmax = None, skipIndex = 0, verbose = False):
     '''
     verbose prints the percent done while running
     skipIndex skips a fixed number of indices during calculation--- 
@@ -59,6 +42,31 @@ def autoCorrelate2(data, Nmax = None, skipIndex = 0, verbose = False):
                 print '{}% done...'.format(k/n*100)
             rho[k] = R/N
 
+    return rho
+
+# use this method -- autoCorrelate1 was used to verify the math was correct
+def autoCorrelate2(data, Nmax = None, skipIndex = 1, verbose = False):
+    '''
+    verbose prints the percent done while running
+    skipIndex skips a fixed number of indices during calculation---
+    this should be used to insure simulations of different dt have
+    the same number of points in calculation
+    '''
+    n = len(data)
+    if Nmax is not None:
+        n = Nmax     # total number of points desired
+    rho = np.zeros(n)
+    mu = np.mean(data)
+
+    percent_done = int(n/100 + 1)
+
+    data_minus_mu = data - mu
+    for k in range(0, n):
+        R = sum(data_minus_mu[0:len(data)-k:skipIndex]*data_minus_mu[k:len(data):skipIndex])
+        N = sum((data_minus_mu**2)[0:len(data)-k:skipIndex])
+        if verbose and k % percent_done == 0:
+            print '{}% done...'.format(k/n*100)
+        rho[k] = R/N
     return rho
 
 if __name__ == "__main__":
