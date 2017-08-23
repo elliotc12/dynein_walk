0e5142b90464e08ab50059b2cf7ef87835be122b
John Waczak <waczakj@oregonstate.edu>
2017-08-23 11:55:46 -0700

    dt1e-12 scripts

--- timeCorrelationTests/dt_graph.py
+++ timeCorrelationTests/dt_graph.py
@@ -7,9 +7,11 @@ label = sys.argv[1]
 
 data_10 = np.loadtxt('./data/onebound_dt1e-10.txt', delimiter=',', skiprows = 1)
 data_11 = np.loadtxt('./data/onebound_dt1e-11.txt', delimiter=',', skiprows = 1)
+data_12 = np.loadtxt('./data/onebound_dt1e-12.txt', delimiter=',', skiprows = 1) 
 
 d10 = {}
 d11 = {}
+d12 = {}
 
 d10['t'] = data_10[:,0]
 d10['rho1'] = data_10[:,1]
@@ -25,6 +27,13 @@ d11['rho3'] = data_11[:,3]
 d11['rho4'] = data_11[:,4]
 d11['rho5'] = data_11[:,5]
 
+d12['t'] = data_12[:,0]
+d12['rho1'] = data_12[:,1]
+d12['rho2'] = data_12[:,2]
+d12['rho3'] = data_12[:,3]
+d12['rho4'] = data_12[:,4]
+d12['rho5'] = data_12[:,5]
+
 plt.figure()
 
 plt.plot(d10['t'], d10['rho1'], 'r', label='rho1 dt1e-10')
@@ -39,7 +48,13 @@ plt.plot(d11['t'], d11['rho3'], 'b--', label = 'rho3 dt1e-11')
 plt.plot(d11['t'], d11['rho4'], 'k--', label = 'rho4 dt1e-11')
 plt.plot(d11['t'], d11['rho5'], 'm--', label = 'rho5 dt1e-11')
 
-plt.title("dt = 1e-10 vs dt = 1e-11")
+plt.plot(d12['t'], d12['rho1'], 'r-.', label = 'rho1 dt1e-12')
+plt.plot(d12['t'], d12['rho2'], 'g-.', label = 'rho2 dt1e-12')
+plt.plot(d12['t'], d12['rho3'], 'b-.', label = 'rho3 dt1e-12')
+plt.plot(d12['t'], d12['rho4'], 'k-.', label = 'rho4 dt1e-12')
+plt.plot(d12['t'], d12['rho5'], 'm-.', label = 'rho5 dt1e-12')
+
+plt.title("dt comparison")
 plt.xlabel('t [s]')
 plt.ylabel(r'$\rho(\Delta t)$')
 plt.legend(loc = 0)
