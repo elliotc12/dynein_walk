c8c1a6a71dada806b183320f8c24a3dd24f3c9c6
David Roundy <roundyd@physics.oregonstate.edu>
2017-10-10 11:54:41 -0700

    remove warning message

--- reorganize/scripts/draw/create_MD_array.py
+++ reorganize/scripts/draw/create_MD_array.py
@@ -20,7 +20,8 @@ with open(outer_path, 'r') as outer_data:
                 f =float(strng)
                 outer_disps.append(f)
             except ValueError:
-                print("Cannot convert string to int", strng)
+                if strng != '':
+                    print("Cannot convert string to int", strng)
 mdo_x = [0]     #motor domain outer path x displacements
 mdo_y = [0]     #motor domain outer path y displacements
 for i in range( len(outer_disps)):
