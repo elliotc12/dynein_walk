3fb33256d416aa560fe1579c5575c7677a47ad3b
elliotc12 <elliotc12@gmail.com>
2017-10-09 20:26:53 -0700

    Make stuff build on lab computers.

--- reorganize/scripts/run_scripts/generate-exploration-data.py
+++ reorganize/scripts/run_scripts/generate-exploration-data.py
@@ -6,16 +6,16 @@ l = "exploration"
 
 simrunner.avoid_roundqueue = True
 
-basename = simrunner.run_sim(**{"k_b": 35, "k_ub": 5e9, "cb": 2, "cm": 2, "ct": 1, "dt": 1e-10, "label": l,
+basename = simrunner.run_sim(**{"k_b": 35*1e9, "k_ub": 5e9, "cb": 2, "cm": 2, "ct": 1, "dt": 1e-10, "label": l,
                                 "seed": 1,
-                                "runtime": 20,
-                                "framerate": 1e-4,
+                                "runtime": 1e-3,
+                                "framerate": 1e-9,
                                 "constant-write": True,
                                 "no-slurm": True})
 
-os.rename("../data/stepping_movie_data_%s.txt" % (basename), "../data/exploration_movie_data.txt")
-os.rename("../data/stepping_data_%s.txt" % (basename), "../data/exploration_stepping_data.txt")
-os.rename("../data/stepping_parameters_%s.tex" % (basename), "../data/exploration_stepping_parameters.tex")
+os.rename("data/stepping_movie_data_%s.txt" % (basename), "data/exploration_movie_data.txt")
+os.rename("data/stepping_data_%s.txt" % (basename), "data/exploration_stepping_data.txt")
+os.rename("data/stepping_parameters_%s.tex" % (basename), "data/exploration_stepping_parameters.tex")
 
-os.unlink("../data/stepping_movie_config_%s.txt" % basename)
-os.unlink("../data/stepping_config_%s.txt" % basename)
+os.unlink("data/stepping_movie_config_%s.txt" % basename)
+os.unlink("data/stepping_config_%s.txt" % basename)
