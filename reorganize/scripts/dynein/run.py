5ff462a3ffb934cd5c2c6558a942ad4358fa11a3
David Roundy <roundyd@physics.oregonstate.edu>
2017-10-13 17:07:53 -0700

    actually create data dir before using it

--- reorganize/scripts/dynein/run.py
+++ reorganize/scripts/dynein/run.py
@@ -64,6 +64,7 @@ def run(**run):
         if key in run:
             cmd.extend(["--"+key])
 
+    os.makedirs('data', exist_ok=True) # ensure data directory exists
     with open("data/stepping_parameters_%s.tex" % basename, "w") as f:
         for k,v in sorted(run.items()):
             if k == "label":
@@ -71,7 +72,6 @@ def run(**run):
             else:
                 f.write(r'\newcommand\%s{%s}' % (latex_format(k).replace("_",""), latex_format(v)) + '\n')
     os.makedirs('runlogs', exist_ok=True) # ensure runlogs directory exists
-    os.makedirs('data', exist_ok=True) # ensure data directory exists
     out = open('runlogs/' + basename + '.out', 'w')
     process_object = subprocess.Popen(cmd, stdout=out, stderr=subprocess.STDOUT)
     print("Running: ", " ".join(cmd))
