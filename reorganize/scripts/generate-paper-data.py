97bd84da3c59ee6218aa30d2c5accb078abc2380
elliotc12 <elliotc12@gmail.com>
2017-11-07 09:46:33 -0800

    Add newest, probably most correct angles to generate-paper-data

--- reorganize/scripts/generate-paper-data.py
+++ reorganize/scripts/generate-paper-data.py
@@ -9,16 +9,16 @@ if 'long' in sys.argv:
     runtime = 100e-3
     l = 'long_paper'
 
-basename  = run.sim(**{"k_b": 1e15,
-                       "k_ub": 1e4,
+basename  = run.sim(**{"k_b": 1e9,
+                       "k_ub": 1e7,
                        "cb": 2.0,
                        "cm": 0.7,
                        "ct": 0.2,
                        "ls": 10.49, # from urnavicius 2015 (paper.bib)
                        "lt": 23.8,  # from urnavicius 2015
                        "eqb": 120,  # from redwine 2012 supplemental
-                       "eqmpre": 136, # from burgess 2002
-                       "eqmpost": 160, # from burgess 2002
+                       "eqmpre": 200, # from burgess 2002, 360-160
+                       "eqmpost": 224, # from burgess 2002, 360-136
                        "eqt": 0,
                        "dt": 1e-10, "label": l, "seed": 1, "runtime": runtime,
                        "framerate": 1e-8, "crash-movie": False,
