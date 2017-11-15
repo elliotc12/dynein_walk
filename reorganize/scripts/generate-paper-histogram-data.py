c275b2d703fecf63cc389cd2293b44e2ced36a5e
elliotc12 <elliotc12@gmail.com>
2017-11-15 00:17:11 -0800

    Add parallelization through a helper script, histogram-helper.py

--- reorganize/scripts/generate-paper-histogram-data.py
+++ reorganize/scripts/generate-paper-histogram-data.py
@@ -7,20 +7,18 @@ import dynein.run as run
 runtime = 2
 
 os.system("make generate_stepping_data")
-os.system("rm data/stepping_data_paperhisto_*")
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("-s", "--seed", type=int, help="Manually set the seed value, default=1")
-# args = parser.parse_args()
-# if args.seed:
-#     seed = args.seed
-# else:
-#     seed = 1
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--seed", type=int, help="Manually set the seed value, default=1")
+args = parser.parse_args()
+if args.seed:
+    seed = args.seed
+else:
+    seed = 1
 
 label = "paperhisto"
-seeds = [1, 2, 3, 4, 5]
 
-params = {"k_b": 1e9,
+run.cmd(**{"k_b": 1e9,
           "k_ub": 100,
           "cb": 0.1,
           "cm": 0.5,
@@ -33,11 +31,7 @@ params = {"k_b": 1e9,
           "eqt": 0,
           "dt": 1e-10, "label": label, "seed": seed, "runtime": runtime,
           "framerate": 1e-5, "crash-movie": False,
-          "no-slurm": True}
+          "no-slurm": True})
 
-for s in seeds:
-    params["seed"] = s
-    run.sim(**params)
-
-os.system("rm data/stepping_movie_config_paperhisto*.txt" % basename)
-os.system("rm data/stepping_config_paperhisto*.txt" % basename)
+os.system("rm data/stepping_movie_config_paperhisto*.txt")
+os.system("rm data/stepping_config_paperhisto*.txt")
