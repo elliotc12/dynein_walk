9303f8157bb1b234fd5cc89a7e16eb57e81e9f01
elliotc12 <elliotc12@gmail.com>
2017-11-16 14:15:10 -0800

    Fix bugs in paper histogram code

--- reorganize/scripts/generate-paper-histogram-data.py
+++ reorganize/scripts/generate-paper-histogram-data.py
@@ -4,7 +4,7 @@ import os, sys
 import numpy as np
 import dynein.run as run
 
-runtime = 2
+runtime = 1
 
 os.system("make generate_stepping_data")
 
@@ -18,22 +18,22 @@ else:
 
 label = "paperhisto"
 
-run.sim(**{"k_b": 1e9,
-          "k_ub": 100,
-          "cb": 0.1,
-          "cm": 0.5,
-          "ct": 0.2,
-          "ls": 10.49, # from urnavicius 2015 (paper.bib)
-          "lt": 23.8,  # from urnavicius 2015
-          "eqb": 120,  # from redwine 2012 supplemental
-          "eqmpre": 200, # from burgess 2002, 360-160
-          "eqmpost": 224, # from burgess 2002, 360-136
-          "eqt": 0,
-          "dt": 1e-10, "label": label, "seed": seed, "runtime": runtime,
-          "framerate": 1, "crash-movie": False})
+basename = run.sim(**{"k_b": 1e9,
+                      "k_ub": 100,
+                      "cb": 0.1,
+                      "cm": 0.5,
+                      "ct": 0.2,
+                      "ls": 10.49, # from urnavicius 2015 (paper.bib)
+                      "lt": 23.8,  # from urnavicius 2015
+                      "eqb": 120,  # from redwine 2012 supplemental
+                      "eqmpre": 200, # from burgess 2002, 360-160
+                      "eqmpost": 224, # from burgess 2002, 360-136
+                      "eqt": 0,
+                      "dt": 1e-10, "label": label, "seed": seed, "runtime": runtime,
+                      "framerate": 1, "crash-movie": False})
 
-os.system("mv data/stepping_data_paperhisto_%s.txt data/paper_histogram_stepping_data-%s.txt", basename, seed)
-os.system("mv data/stepping_parameters_paperhisto_%s.txt data/paper_histogram_stepping_parameters.tex", basename, seed)
-os.system("rm data/stepping_movie_config_paperhisto_%s.txt", basename)
-os.system("rm data/stepping_movie_data_paperhisto_%s.txt", basename)
-os.system("rm data/stepping_config_paperhisto_%s.txt", basename)
+os.system("mv data/stepping_data_%s.txt data/paper_histogram_stepping_data-%s.txt" % (basename, seed))
+os.system("mv data/stepping_parameters_%s.tex data/paper_histogram_stepping_parameters.tex" % basename)
+os.system("rm data/stepping_movie_config_%s.txt" % basename)
+os.system("rm data/stepping_movie_data_%s.txt" % basename)
+os.system("rm data/stepping_config_%s.txt" % basename)
