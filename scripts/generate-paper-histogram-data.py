a0a0ba1d27993262b48872f380e7758efe25a6a7
elliotc12 <elliotc12@gmail.com>
2018-01-01 17:20:32 -0800

    Add feature to generate-paper-histogram to be runnable from other scripts.

--- scripts/generate-paper-histogram-data.py
+++ scripts/generate-paper-histogram-data.py
@@ -9,31 +9,32 @@ runtime = 1e-3
 os.system("make generate_stepping_data")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--seed", type=int, help="Manually set the seed value, default=1")
+parser.add_argument("-s", "--seed", type=int, help="Manually set the seed value, default=1", default=1)
+parser.add_argument("-b", "--cb", type=float, help="Manually set the cb value, default=0.1", default=0.1)
+parser.add_argument("-m", "--cm", type=float, help="Manually set the cm value, default=0.4", default=0.4)
+parser.add_argument("-t", "--ct", type=float, help="Manually set the ct value, default=0.2", default=0.2)
+parser.add_argument("-l", "--label", type=str, help="Manually set the label", default="paperhisto")
+parser.add_argument("-p", "--notpaper", type=bool, help="Not making paper plot", default=False)
 args = parser.parse_args()
-if args.seed:
-    seed = args.seed
-else:
-    seed = 1
 
-label = "paperhisto"
-
-basename = run.sim(**{"k_b": 1e10,
-                      "k_ub": 1e10,
-                      "cb": 0.1,
-                      "cm": 0.4,
-                      "ct": 0.2,
+basename = run.sim(**{"k_b": 1e9, # 1e9
+                      "k_ub": 1e8, # 100
+                      "cb": args.cb, # 0.1
+                      "cm": args.cm, # 0.4
+                      "ct": args.ct, # 0.2
                       "ls": 10.49, # from urnavicius 2015 (paper.bib)
                       "lt": 23.8,  # from urnavicius 2015
                       "eqb": 120,  # from redwine 2012 supplemental
                       "eqmpre": 200, # from burgess 2002, 360-160
                       "eqmpost": 224, # from burgess 2002, 360-136
                       "eqt": 0,
-                      "dt": 1e-10, "label": label, "seed": seed, "runtime": runtime,
+                      "dt": 1e-10, "label": args.label, "seed": args.seed, "runtime": runtime,
                       "framerate": 1, "crash-movie": False})
 
-os.system("mv data/stepping_data_%s.txt data/paper_histogram_stepping_data-%s.txt" % (basename, seed))
-os.system("mv data/stepping_parameters_%s.tex data/paper_histogram_stepping_parameters.tex" % basename)
+if not args.notpaper:
+    os.system("mv data/stepping_data_%s.txt data/paper_histogram_stepping_data-%s.txt" % (basename, args.seed))
+    os.system("mv data/stepping_parameters_%s.tex data/paper_histogram_stepping_parameters.tex" % basename)
+
 os.system("rm data/stepping_movie_config_%s.txt" % basename)
 os.system("rm data/stepping_movie_data_%s.txt" % basename)
 os.system("rm data/stepping_config_%s.txt" % basename)
