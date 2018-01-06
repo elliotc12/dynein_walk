0b253ec583318f087a0753d0efb19173cb3ca9a5
elliotc12 <elliotc12@gmail.com>
2018-01-06 03:02:15 -0800

    Allow custom labels to springsearch.py

--- scripts/springsearch.py
+++ scripts/springsearch.py
@@ -6,22 +6,19 @@ os.system("mkdir -p plots/springsearch")
 seeds = [1, 2, 3, 4]
 
 sims = []
-sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.01"})
-sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.1"})
-sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.2"})
-sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.3"})
-sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.4"})
-
-simnum = 0
+sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.05", "num" : 5})
+sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.8" , "num" : 6})
+sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.12" , "num" : 7})
+sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.15" , "num" : 8})
+sims.append({"cb" : "0.1", "cm" : "0.4", "ct" : "0.6" , "num" : 9})
 
 for sim in sims:
     for s in seeds:
-        os.system("rq run --job-name springsearch-" + str(simnum) \
+        os.system("rq run --job-name springsearch-" + str(sim["num"]) \
                   + " python3 scripts/generate-paper-histogram-data.py" \
                   + " --cb " + sim["cb"] \
                   + " --cm " + sim["cm"] \
                   + " --ct " + sim["ct"] \
                   + " --seed " + str(s) \
                   + " --notpaper" \
-                  + " --label springsearch-" + str(simnum))
-    simnum += 1
+                  + " --label springsearch-" + str(sim["num"]))
