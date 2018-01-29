55b41f40e5f8443c6d9994bfbc9483ad1e0909ee
John Waczak <waczakj@oregonstate.edu>
2018-01-28 22:24:16 -0800

    trying to get -a to work again

--- scripts/color_hist.py
+++ scripts/color_hist.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
-import os, glob
+import os
+import glob
+
 
 parser = argparse.ArgumentParser(description = 'Script to generate 2 dimensional histogram from dynein stepping data')
 
