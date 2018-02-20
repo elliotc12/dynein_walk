c5547e40189c954b9c1a062927d0d656d1ea7d10
John Waczak <waczakj@oregonstate.edu>
2018-02-20 12:09:18 -0800

    testing parameters

--- scripts/parameter_search.py
+++ scripts/parameter_search.py
@@ -6,8 +6,8 @@ import glob
 import numpy as np
 
 
-k_b = 300000000.0
-k_ub = 50
+k_b = 1e9
+k_ub = 10
 runtime = 0.5
 exp_binding_constant = 0
 
@@ -37,7 +37,9 @@ basename = run.sim(**{"k_b": k_b,
                       "exp_binding_constant": exp_binding_constant})
 
 
-dataFile = glob.blob("data/stepping_data_paramSearch*.txt")
+os.chdir("data/")
+dataFile = glob.glob("stepping_data_paramSearch*.txt")
+os.chdir("../")
 if len(dataFile) is not 1:
     print("Something went wrong. Make sure data was generated and that old paramSearch files have been deleted or renamed")
 
@@ -46,7 +48,7 @@ onebound_times = []
 bothbound_times = []
 step_lengths = []
 
-data = np.loadtxt(dataFile)
+data = np.loadtxt("data/"+dataFile[0])
 print("Data loaded...")
 bind_times = np.array(data[:,1])
 unbind_times = np.array(data[:,0])
@@ -76,11 +78,15 @@ fb_disp = far_positions[-1]-far_positions[0]
 print("Writing to testedParameters.txt")
 
 if not os.path.exists("data/testedParameters.txt"):
-    os.chdir("/data")
+    os.chdir("data/")
     os.system("touch testedParameters.txt")
-    os.chdir("../")
+
 
 with open("data/testedParameters.txt", "a") as file:
-    file.write(k_b, k_ub, runtime, exp_binding_constant, max_ob_t, min_ob_t, max_bb_t, min_bb_t, max_nb_step, min_nb_step, max_fb_step, min_fb_step, total_steps, nb_disp, fb_disp)
+    file.write("{0}, \t{1}, \t{2}, \t{3}, \t{4}, \t{5}, \t{6}, \t{7}, \t{8}, \t{9}, \t{10}, \t{11}, \t{12}, \t{13}, \t{14}".format(k_b, k_ub, runtime, exp_binding_constant, max_ob_t, min_ob_t, max_bb_t, min_bb_t, max_nb_step, min_nb_step, max_fb_step, min_fb_step, total_steps, nb_disp, fb_disp))
+
+print(os.getcwd())
+os.chdir("data/")
+os.system("mv ./{3} ./kb{0}_kub{1}_expbc{2}.txt".format(k_b, k_ub, exp_binding_constant, dataFile[0]))
 
 print("All done.")
