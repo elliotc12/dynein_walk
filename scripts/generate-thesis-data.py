a19996211f944703d2dcbc4c99ff7a4a97229d41
elliotc12 <elliotc12@gmail.com>
2018-04-16 01:35:47 -0700

    minor bugfix to hopefully make make make again

--- scripts/generate-thesis-data.py
+++ scripts/generate-thesis-data.py
@@ -14,8 +14,9 @@ basename  = run.sim(**{"k_b": 1e15,
                        "ct": 0.5,
                        "ls": 22.1,
                        "lt": 11.15,
+                       "exp-unbinding-constant": 0.0,
                        "dt": 1e-10, "label": l, "seed": 1, "runtime": 1e-3, "framerate": 1e-8,
-                       "crash-movie": False})
+                       "full-gibbs-transitions": True, "crash-movie": False})
 
 #this one has a bothbound at the end
 # basename = simrunner.run_sim(**{"k_b": 1e18, "k_ub": 1e7, "cb": 2.0, "cm": 2.0, "ct": 1.0, "dt": 1e-10, "label": l,
