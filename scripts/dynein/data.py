0c67fa34877d71a0e369e2e0e7370e736cbe5741
David Roundy <roundyd@physics.oregonstate.edu>
2018-05-03 10:00:14 -0700

    increase decimals in data.py does not matter much

--- scripts/dynein/data.py
+++ scripts/dynein/data.py
@@ -8,12 +8,12 @@ class SteppingData(object):
         # assert(np.shape(self.rawData)[1] == 4)  # guarantee 4 columns in data file
         self.bindTimes = self.rawData[:, 1]
         self.unbindTimes = self.rawData[:, 0]
-        self.nbx_bind = np.around(self.rawData[:, 2], decimals=7)
-        self.fbx_bind = np.around(self.rawData[:, 3], decimals=7)
-        self.nmx_unbind = np.around(self.rawData[:, 4], decimals=7)
-        self.fmx_unbind = np.around(self.rawData[:, 5], decimals=7)
-        self.nmx_bind = np.around(self.rawData[:, 6], decimals=7)
-        self.fmx_bind = np.around(self.rawData[:, 7], decimals=7)
+        self.nbx_bind = np.around(self.rawData[:, 2], decimals=12)
+        self.fbx_bind = np.around(self.rawData[:, 3], decimals=12)
+        self.nmx_unbind = np.around(self.rawData[:, 4], decimals=12)
+        self.fmx_unbind = np.around(self.rawData[:, 5], decimals=12)
+        self.nmx_bind = np.around(self.rawData[:, 6], decimals=12)
+        self.fmx_bind = np.around(self.rawData[:, 7], decimals=12)
 
         self.near_step_len = self.nbx_bind[1:]-self.nbx_bind[:-1]
         self.far_step_len = self.fbx_bind[1:]-self.fbx_bind[:-1]
@@ -26,8 +26,11 @@ class SteppingData(object):
 
         assert(len(self.nbx_bind)==len(self.fbx_bind))
         for s in range(1, len(self.nbx_bind)):
-            # print(self.nbx_bind[s-1]-self.nbx_bind[s], self.fbx_bind[s-1]-self.fbx_bind[s])
-            assert((self.nbx_bind[s-1] == self.nbx_bind[s]) or (self.fbx_bind[s-1] == self.fbx_bind[s]))
+            # print('near', self.nbx_bind[s-1], self.nbx_bind[s],  self.nbx_bind[s-1]-self.nbx_bind[s])
+            # print('far', '%.19g' % self.fbx_bind[s-1], '%.19g' % self.fbx_bind[s],
+            #       self.fbx_bind[s-1]-self.fbx_bind[s])
+            assert((self.nbx_bind[s-1] == self.nbx_bind[s])
+                   or (self.fbx_bind[s-1] == self.fbx_bind[s]))
             if(self.nbx_bind[s-1]== self.nbx_bind[s] and self.fbx_bind[s-1] == self.nbx_bind[s]):
                 continue
             if not (self.nbx_bind[s-1] == self.nbx_bind[s]):
@@ -48,7 +51,11 @@ class MovieData:
 
 
 if __name__ == "__main__":
-    data = SteppingData("data/parameterSearch/kb100000000.0_kub100_expbc-0.5_t5.0_seed1.0.txt")
+    import sys
+    if len(sys.argv) == 2:
+        data = SteppingData(sys.argv[1])
+    else:
+        data = SteppingData("data/parameterSearch/kb100000000.0_kub100_expbc-0.5_t5.0_seed1.0.txt")
     print(data.rawData)
     print(data.initial_displacements)
     print(data.final_displacements)
