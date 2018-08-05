b06191bc019967b38778d860cf90b992818b0c7a
Elliott Capek <elliotc12@gmail.com>
2018-08-05 14:15:09 -0400

    Change paper_params eq angles and lengths to better match the burgess cryo-em figure; using model fitting in lieu of values reported from paper

--- data/paper_params.py
+++ data/paper_params.py
@@ -12,14 +12,14 @@ cb = 0.1 # kbT
 cm = 0.4 # kbT
 ct = 0.2 # kbT
 
-eqb = 116.5
-eqmpre = 200 # 204.5 from burgess
-eqmpost = 224 # 222.5 from burgess, but as much as 234.5 -- they say 15nm difference for pre/post ub domain, so maybe latter is better
+eqb = 128
+eqmpre = 197 # 204.5 from burgess
+eqmpost = 242 # 222.5 from burgess, but as much as 234.5 -- they say 15nm difference for pre/post ub domain, so maybe latter is better
 eqt = 0
 
-ls = 19 # nm
-lt = 23.8 # nm
+ls = 20.75 # nm
+lt = 23 # nm
 
-radius_t = 2.16 # nm
-radius_m = 7.36 # nm
-radius_b = 1.57 # nm
+radius_t = 8 # nm
+radius_m = 11 # nm
+radius_b = 3.5 # nm
