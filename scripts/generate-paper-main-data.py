71de08b580fcfff0615d8f619f80266a29f5c069
elliotc12 <elliotc12@gmail.com>
2018-09-01 06:55:43 -0700

    mini commit

--- scripts/generate-paper-main-data.py
+++ scripts/generate-paper-main-data.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.getcwd() + "/data/")
 import paper_params as params
 
 runtime = 5
-seeds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+seeds = [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
 
 for s in seeds:
     os.system("rq run --job-name papermain-" + str(s) \
