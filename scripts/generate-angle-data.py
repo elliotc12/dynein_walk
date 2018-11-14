15cae1f8478314ec4c5269f8f556e444a136fcab
Elliott Capek <elliotc12@gmail.com>
2018-11-14 14:03:55 -0500

    Add file which should have been in last commit for running the anglemode simulation

---
+++ scripts/generate-angle-data.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import os
+import dynein.run as run
+import glob
+import numpy as np
+import argparse
+import subprocess
+import sys
+
+sys.path.insert(0, os.getcwd() + "/data/")
+import paper_params as params
+
+runtime = 1e-4
+framerate = 1e-10
+
+basename = "paper_stroke_angles"
+
+os.system("python3 scripts/generate-stepping-data.py" \
+          + " --ls " + str(params.ls)\
+          + " --lt " + str(params.lt)\
+          + " --kub " + str(params.stroke_angle_k_ub)\
+          + " --kb  " + str(params.stroke_angle_k_b)\
+          + " --cb " + str(params.cb)\
+          + " --cm " + str(params.cm)\
+          + " --ct " + str(params.ct)\
+          + " --eqb " + str(params.eqb)\
+          + " --eqt " + str(params.eqt)\
+          + " --eqmpre " + str(params.eqmpre)\
+          + " --eqmpost " + str(params.eqmpost)\
+          + " --unbindingconst " + str(params.cexp)\
+          + " --runtime " + str(runtime)\
+          + " --framerate " + str(framerate)\
+          + " --seed " + str(params.stroke_angle_seed)\
+          + " --label paper_stroke_angles"\
+          + " --renameangles"\
+          + " --anglemode")
