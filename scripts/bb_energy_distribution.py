adb14fc08d8b37de6dd473a26117bcc932f13193
john-waczak <john.louis.waczak@gmail.com>
2019-02-28 13:52:18 -0800

    new energy distribution plots!

--- scripts/bb_energy_distribution.py
+++ scripts/bb_energy_distribution.py
@@ -16,8 +16,10 @@ def spring_energy(theta, theta_eq, c): #domain angle, equilibrium angle, spring 
     """
     return 0.5*c*(theta-theta_eq)**2
 
-def get_energy_landscape(nma, fma, L=16): # length in nm, near motor angle, far motor angle
+def get_bb_total_energy(nma, fma, L=16): # length in nm, near motor angle, far motor angle
     """ Calculate total energy for both bound dynein given motor angles and binding domain displacement."""
+
+    # calculate all of the angles 
     Lt = params.for_simulation['lt']
     Ls = params.for_simulation['ls']
     Ln = np.sqrt(Lt**2+Ls**2-2*Lt*Ls*np.cos(nma)) # squared lengths
@@ -36,13 +38,85 @@ def get_energy_landscape(nma, fma, L=16): # length in nm, near motor angle, far 
 
     ta = np.arccos(1-(Lm**2)/(2*Lt**2))
 
+    # calculate all of the energies
     E_t = spring_energy(ta, params.for_simulation['eqt'], params.for_simulation['ct'])
-    print(E_t)
+    E_nm = spring_energy(nma, params.for_simulation['eqmpost'], params.for_simulation['cm'])
+    E_fm = spring_energy(fma, params.for_simulation['eqmpost'], params.for_simulation['cm'])
+    E_nb = spring_energy(nba, params.for_simulation['eqb'], params.for_simulation['cb'])
+    E_fb = spring_energy(fba, params.for_simulation['eqb'], params.for_simulation['cb'])
 
-def remove_nans():
-    pass
+    E_total = E_t+E_nm+E_fm+E_nb+E_fb
+
+    return E_total
+
+def remove_nans(E_total, nma, fma):
+    nan_locations = np.isnan(E_total)
+    print(row_nans)
 
 
+def find_energy_extema(E_total):
+    """
+    Return indices of the maximum and minimum energy value
+    """
+    # need to be careful about nan values
+    max_coords = np.where(E_total==np.nanmax(E_total))
+    min_coords = np.where(E_total==np.nanmin(E_total))
+    # return a list with the correct indices
+    maximum = [max_coords[0][0], max_coords[1][0]]
+    minimum = [min_coords[0][0], min_coords[1][0]]
+    print(maximum)
+    return maximum, minimum
 
+
+def plot_bb_energy_distribution(nma, fma, L=16, extrema=True):
+    NMA, FMA = np.meshgrid(nma, fma)
+    E_total = get_bb_total_energy(NMA, FMA, L)
+
+    fig = plt.figure()
+    ax = plt.gca()
+    ax.pcolor(NMA, FMA, E_total)
+    ax.set_xlabel('Near motor angle')
+    ax.set_ylabel('Far motor angle')
+    ax.set_title('Total Energy distribution for L={0}'.format(L))
+
+    if extrema == True:
+        extrema = np.asarray(find_energy_extema(E_total))
+        x = nma[extrema[:,0]]
+        y = fma[extrema[:, 1]]
+        ax.scatter(x,y, color='r')
+
+    return fig, ax
 if __name__ == "__main__":
-    get_energy_landscape(nma, fma)
+   fig1, ax1 = plot_bb_energy_distribution(nma, fma, L=8, extrema=True)
+   fig2, ax2 = plot_bb_energy_distribution(nma, fma, L=16)
+   fig3, ax3 = plot_bb_energy_distribution(nma, fma, L=24)
+   plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
