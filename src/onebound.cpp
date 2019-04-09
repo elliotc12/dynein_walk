896883bf943b054243f36ede4ea68011da4600f4
David Roundy <roundyd@physics.oregonstate.edu>
2019-04-09 11:18:22 -0700

    beautify code and add just a bit of printing

--- src/onebound.cpp
+++ src/onebound.cpp
@@ -18,13 +18,13 @@
 
 
 int main() {
-  double bba = 0, bma = 0, fma = 0, fba = 0, bbx = 0, bby = 4; // FIXME
+  double bba = 0.3, bma = 0.5, fma = 0.25, fba = 0.15, bbx = 0, bby = 4; // FIXME
 
   // we will probably need Jin's code to output which foot steps
   // do we care about nearbound or farbound
 
   MTRand* rand = new MTRand(0.0); // FIXME
-  Dynein_onebound* dyn_ob = new Dynein_onebound(bba, bma, fma, fba, bbx, bby,
+  Dynein_onebound* dynein = new Dynein_onebound(bba, bma, fma, fba, bbx, bby,
                                                 NEARBOUND,
                                                 NULL,
                                                 NULL,
@@ -37,32 +37,27 @@ int main() {
 
   while(stillStepping){
 
-    double binding_prob = dyn_ob->get_binding_rate()*dt;
-    double unbinding_prob = dyn_ob->get_unbinding_rate()*dt;
+    double binding_prob = dynein->get_binding_rate()*dt;
+    double unbinding_prob = dynein->get_unbinding_rate()*dt;
+    if (binding_prob > 0) printf("binding_prob is %g\n", binding_prob);
 
     // deal with falling off of microtubule
     if (rand->rand() < unbinding_prob) {
-      delete dyn_ob;
-      dyn_ob = NULL;
       printf("I fell off of the microtubule!");
       exit(1);
-    }
-
-
-    //
-    else if (rand->rand() < binding_prob){ // bind condition
-      printf("I took a step! Final L = %f", dyn_ob->get_bbx()-dyn_ob->get_ubx());
+    } else if (rand->rand() < binding_prob) {
+      // We are going to bind!
+      printf("I took a step! Final L = %f", dynein->get_bbx()-dynein->get_ubx());
       exit(0);
-    }
-
-    else{
+    } else {
       t += dt; // iterate time
       iter ++;
 
-      double old_bba = dyn_ob->get_bba();
-      double old_bma = dyn_ob->get_bma();
-      double old_uba = dyn_ob->get_uba();
-      double old_uma = dyn_ob->get_uma();
+      double old_bba = dynein->get_bba();
+      double old_bma = dynein->get_bma();
+      double old_uba = dynein->get_uba();
+      double old_uma = dynein->get_uma();
+      if (binding_prob > 0) printf("binding domain at %g %g\n", dynein->get_ubx(), dynein->get_uby());
 
 
       bool accept_step = false;
@@ -74,22 +69,22 @@ int main() {
           printf("Over %lld attempts needed to avoid a NaN state in onebound at time %g, something must be wrong. Exiting.\n", max_attempts, t);
           exit(1);
         }
-        dyn_ob->set_bba(old_bba);
-        dyn_ob->set_bma(old_bma);
-        dyn_ob->set_uba(old_uba);
-        dyn_ob->set_uma(old_uma);
+        dynein->set_bba(old_bba);
+        dynein->set_bma(old_bma);
+        dynein->set_uba(old_uba);
+        dynein->set_uma(old_uma);
 
-        double temp_bba = dyn_ob->get_bba() + dyn_ob->get_d_bba() * dt;
-        double temp_bma = dyn_ob->get_bma() + dyn_ob->get_d_bma() * dt;
-        double temp_uma = dyn_ob->get_uma() + dyn_ob->get_d_uma() * dt;
-        double temp_uba = dyn_ob->get_uba() + dyn_ob->get_d_uba() * dt;
+        double temp_bba = dynein->get_bba() + dynein->get_d_bba() * dt;
+        double temp_bma = dynein->get_bma() + dynein->get_d_bma() * dt;
+        double temp_uma = dynein->get_uma() + dynein->get_d_uma() * dt;
+        double temp_uba = dynein->get_uba() + dynein->get_d_uba() * dt;
 
-        dyn_ob->set_bba(temp_bba);
-        dyn_ob->set_bma(temp_bma);
-        dyn_ob->set_uma(temp_uma);
-        dyn_ob->set_uba(temp_uba);
+        dynein->set_bba(temp_bba);
+        dynein->set_bma(temp_bma);
+        dynein->set_uma(temp_uma);
+        dynein->set_uba(temp_uba);
 
-        accept_step = dyn_ob->update_velocities(); //NOTE: double check why this is a bool and not void
+        accept_step = dynein->update_velocities(); //NOTE: double check why this is a bool and not void
         attempts++;
       }
     }
