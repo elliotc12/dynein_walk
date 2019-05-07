ad4bcb6081057bcb001aa71f44278668efdc1823
john-waczak <john.louis.waczak@gmail.com>
2019-05-07 10:10:09 -0700

    thesis update

---
+++ papers/waczak-thesis/thesis/chapters/auto/results.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "results"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:trajectory"
+    "Fig:statistics"
+    "fig:displacement_vs_step_length"
+    "fig:energy_landscape"))
+ :latex)
+
