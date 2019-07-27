21eedb0bd83c6196b1862ec2a97b826ad0b3ec8e
Jin Kiatvongcharoen <kiatvonj@darcy.physics.oregonstate.edu>
2019-07-26 17:07:20 -0700

    Making script to plot all this data

--- scripts/make_bb_mc_plots.py
+++ scripts/make_bb_mc_plots.py
@@ -8,6 +8,7 @@ import importlib
 import argparse
 import subprocess
 import bb_energy_distribution
+from mpl_toolkits.mplot3d import Axes3D
 
 params = importlib.import_module("params")
 
@@ -17,8 +18,43 @@ parser.add_argument("-k", "--kb", type=float, help="binding rate", required=True
 parser.add_argument("-t", "--dt", type=float, help="dt", required=True)
 args = parser.parse_args()
 
+def rad_to_deg(angle):
+    # array = angle*180/np.pi
+    return angle
+
 data_file = open("../data/mc_data_{0}_{1}_{2}.txt".format(int(args.L), args.kb, args.dt), "r")
 
-print(data_file.readline(6000))
+data = data_file.readlines()
+init_ang = [[] for i in range(2)]
+final_L = []
+final_ang = [[] for i in range(2)]
+step_length = []
+
+for i in range(3, 100):
+    line = data[i].split("\t")
+    init_ang[0].append(line[1])
+    init_ang[1].append(line[2])
+    final_L.append(line[3])
+    final_ang[0].append(line[4])
+    final_ang[1].append(line[5])
+    step_length.append(line[6])
+
+fig = plt.figure(figsize=(10,15))
+
+# make contourf graph
+ax1 = fig.add_subplot(1,1,1, projection='3d')
+FinalLPlot = ax1.scatter(init_ang[0], init_ang[1], final_L, c='r', marker='o')
+# contour = ax1.contour(rad_to_deg(init_ang[0]), rad_to_deg(init_ang[1]), final_L, np.linspace(0, 1, 5), colors='w', linewidth=10)
+ax1.set_xlabel(r'$\theta_{nm}$', fontsize=60)
+ax1.set_ylabel(r'$\theta_{fm}$', fontsize=60)
+ax1.set_zlabel(r'Final L')
+ax1.set_xticks(np.linspace(0, 360, 13))
+ax1.set_yticks(np.linspace(0, 360, 13))
+# cb = plt.colorbar(FinalLPlot)
+# cb.set_label(r"Final L", fontsize=40)
+# cb.set_ticks(np.linspace(0, 1, 5))
+# cb.add_lines(contour)
+
+plt.show()
 
 data_file.close()
