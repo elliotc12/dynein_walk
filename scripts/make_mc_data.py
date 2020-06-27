cf708e72fd485bb33f238400b6cfb354142897d3
Jin Kiatvongcharoen <kiatvonj@darcy.physics.oregonstate.edu>
2020-06-27 01:13:50 -0700

    Made compressed data file carry all ob data

--- scripts/make_mc_data.py
+++ scripts/make_mc_data.py
@@ -15,80 +15,6 @@ import subprocess
 import bb_energy_distribution
 from glob import glob
 
-def integrate_1d(arr, dx):
-    sum = 0
-    for i in range(len(arr)):
-        sum += arr[i]*dx[i]
-    return sum
-
-def integrate_2d(arr, dx, dy):
-    sum = 0
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            sum += arr[i,j]*dx[j]*dy[i]
-    return sum
-
-def least_squares(arr, x, y, dx, dy):
-    # X,Y = np.meshgrid(x,y)
-    # x_mean = integrate_2d(arr*X, dx, dy)
-    # y_mean = integrate_2d(arr*Y, dx, dy)
-    # x2_mean = integrate_2d(arr*X**2, dx, dy)
-    # xy_mean = integrate_2d(arr*X*Y, dx, dy)
-    x_mean = 0
-    y_mean = 0
-    x2_mean = 0
-    xy_mean = 0
-
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            x_mean += arr[i,j]*x[j]*dx[i]*dy[j]
-            y_mean += arr[i,j]*x[i]*dx[i]*dy[j]
-            x2_mean += arr[i,j]*y[j]**2*dx[i]*dy[j]
-            xy_mean += arr[i,j]*y[j]*x[i]*dx[i]*dy[j]
-
-    # Intercept & Slope for Best-fit
-    b = (y_mean*x2_mean-xy_mean*x_mean)/(x2_mean-x_mean**2)
-    m = (-b*x_mean+xy_mean)/(x2_mean)
-    lin_fit = (m*x)+b
-
-    return b, m, lin_fit
-
-
-def L_to_initial_displacement(P_leading, P_trailing):
-    """
-    Returns a matrix (technically array) which when multiplied by a
-    probability density of L will result in a probability density of
-    displacement.  Thus this is a dimensionless 2D array.
-
-    """
-    num_col = 2*len(P_leading)
-    num_rows = len(P_leading)
-    P_step = np.zeros((num_col,num_rows))
-    for i in range(num_col):
-        if i < num_col/2:
-            P_step[num_rows-1-i,i] = P_trailing[num_rows-1-i]              # P_trailing array starts at 1 to 50
-            P_step[num_rows+i, i] = P_leading[num_rows-1-i]        # P_leading array starts at 1 to 50
-    return P_step
-
-def L_to_L(T, P_leading, P_trailing):
-    num_col = 2*len(P_leading)
-    num_rows = len(P_leading)
-    abs = np.zeros((num_rows,num_col))
-    for i in range(num_col):
-        if i < num_col/2:
-            abs[num_rows-1-i,i] = 1
-        else:
-            abs[i-num_rows,i] = 1
-    prob_step = L_to_initial_displacement(P_leading, P_trailing)
-    T_L = abs*T*prob_step
-    # plt.figure('abs')
-    # plt.pcolor(abs);
-    # plt.colorbar();
-    # plt.figure('prob_step')
-    # plt.pcolor(prob_step);
-    # plt.colorbar();
-    # plt.show()
-    return T_L
 
 params = importlib.import_module("params")
 
@@ -103,9 +29,7 @@ k_stk = float(args.ks)      # Sticky Rate Constant
 
 basepath = '../data/mc_data_{0:.2e}_{1:.2e}/'.format(k_b, k_stk)
 plotpath = '../plots/mc_plots/'
-final_disp_path = '../data/compressed_mc_data/final_disp_{0:.2e}_{1:.2e}'.format(k_b, k_stk)
-ob_time_path = '../data/compressed_mc_data/ob_time_{0:.2e}_{1:.2e}'.format(k_b, k_stk)
-prob_unbinding_path = '../data/compressed_mc_data/prob_unbinding_{0:.2e}_{1:.2e}'.format(k_b, k_stk)
+datapath = '../data/compressed_mc_data/mc_data_file_{0:.2e}_{1:.2e}'.format(k_b, k_stk)
 leading_files = glob('{}/l_*.txt'.format(basepath))
 
 initial_disp = []
@@ -177,6 +101,4 @@ if len(P_leading) == 0:
 print(final_disp_dict)
 print(ob_time_dict)
 print(P_unbinding)
-np.savez_compressed(final_disp_path, final_disp_dict)
-np.savez_compressed(ob_time_path, ob_time_dict)
-np.savez_compressed(prob_unbinding_path, P_unbinding)
+np.savez_compressed(datapath, final_disp_dict=final_disp_dict, ob_time_dict=ob_time_dict, P_unbinding=P_unbinding)
