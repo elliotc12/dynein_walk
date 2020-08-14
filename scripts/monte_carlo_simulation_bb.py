2ca6f947a241319fe4ad86dd8b71372decdf4e65
Ryan Wong <wongryan@oregonstate.edu>
2020-08-13 17:02:20 -0700

    changed ta in both bound angle sampling, added plotting, need to fix fba.

--- scripts/monte_carlo_simulation_bb.py
+++ scripts/monte_carlo_simulation_bb.py
@@ -53,7 +53,7 @@ def collect_bothbound_data(k, self, P, nma, fma, prob):
 params = importlib.import_module("params")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-L", "--L", type=float, help="displacement in nm", default=np.arange(1,51,1))
+parser.add_argument("-L", "--L", type=float, help="displacement in nm", default= [8])# np.arange(1,51,1))
 parser.add_argument("-N", "--N", type=float, help="how many steps to do", default=1e9)
 parser.add_argument("-u", "--kub", type=float, help="Manually set the unbinding const", default=params.for_simulation['k_ub'])
 parser.add_argument("-k", "--kb", type=float, help="Manually set the binding const", default=params.for_simulation['k_b'])
@@ -132,12 +132,12 @@ for i in range(len(L_arr)):
             if np.isnan(dynein.E_total) == True:
                 continue
             else:
-                print('nba: ', dynein.nba*180/np.pi)
-                print('nma: ', dynein.nma*180/np.pi)
-                print('ta: ', dynein.ta*180/np.pi)
-                print('fma: ', dynein.fma*180/np.pi)
-                print('fba: ', dynein.fba*180/np.pi)
-                print()
+                # print('nba: ', dynein.nba*180/np.pi)
+                # print('nma: ', dynein.nma*180/np.pi)
+                # print('ta: ', dynein.ta*180/np.pi)
+                # print('fma: ', dynein.fma*180/np.pi)
+                # print('fba: ', dynein.fba*180/np.pi)
+                # print()
                 # Calculating partition function
                 P = np.exp(-b*dynein.E_total)
                 Z += P
