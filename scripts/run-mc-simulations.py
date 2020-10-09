400d045e5d9410afc6614c260e3f90aa4bd3165f
Jin Kiatvongcharoen <kiatvonj@darcy.physics.oregonstate.edu>
2020-10-09 15:11:19 -0700

    Changed one more naming scheme

--- scripts/run-mc-simulations.py
+++ scripts/run-mc-simulations.py
@@ -15,6 +15,9 @@ parser.add_argument("-i", "--i", type=float, help="Increment", default=5)
 parser.add_argument("-o", "--o", type=float, help="Starting value", default=1)
 parser.add_argument("-k", "--kb", type=float, help="binding const", default=params.for_simulation['k_b'])
 parser.add_argument("-s", "--ks", type=float, help="sticky const", default=params.for_simulation['k_stk'])
+parser.add_argument("-cb", "--cb", type=float, help="spring const binding domain", default=params.for_simulation['cb'])
+parser.add_argument("-cm", "--cm", type=float, help="spring const motor domain", default=params.for_simulation['cm'])
+parser.add_argument("-ct", "--ct", type=float, help="spring const tail domain", default=params.for_simulation['ct'])
 parser.add_argument("-c", "-cancel", type=bool, help="cancel all jobs", default=False)
 args = parser.parse_args()
 
@@ -28,8 +31,8 @@ init_L = np.arange(o, L, i, dtype=int)
 
 if args.c == True:
     for i in range(len(init_L)):
-        os.system('rq cancel -J dynein-mc-{0:.1e}-{1:.1e}-{2}'.format(k_b, k_stk, init_L[i]))
+        os.system('rq cancel -J dynein-mc-{0:.1e}-{1:.1e}-{2}-{3}-{4}-{5}'.format(k_b, k_stk, args.cb, args.cm, args.ct, init_L[i]))
 else: 
     for i in range(len(init_L)):
-        os.system('rq run -J dynein-mc-{0:.1e}-{1:.1e}-{2} -o L-{3:.1e}-{4:.1e}-{5} python3 monte_carlo_simulation.py -k {6} -s {7} -L {8}'.format(k_b, k_stk, init_L[i], k_b, k_stk, init_L[i], k_b, k_stk, init_L[i]))
+        os.system('rq run -J dynein-mc-{0:.1e}-{1:.1e}-{2}-{3}-{4}-{5} -o L-{6:.1e}-{7:.1e}-{8}-{9}-{10}-{11} python3 monte_carlo_simulation.py -k {12} -s {13} -cb {14} -cm {15} -ct {16} -L {17}'.format(k_b, k_stk, args.cb, args.cm, args.ct, init_L[i], k_b, k_stk, args.cb, args.cm, args.ct, init_L[i], k_b, k_stk, args.cb, args.cm, args.ct, init_L[i]))
         time.sleep(0.1)
